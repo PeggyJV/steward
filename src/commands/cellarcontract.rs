6512522be92caf8b64c1fc1b72d87323a5ef9473
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-06-06 14:44:40 +0100

    Fixing ABI and some cellar methods

--- src/commands/cellarcontract.rs
+++ src/commands/cellarcontract.rs
@@ -1,4 +1,4 @@
-//! `transfer` subcommand - this subcommand transfers ethereum from one account to another
+//! `cellarcontract` subcommand - this subcommand transfers ethereum from one account to another
 use crate::application::APP;
 /// App-local prelude includes `app_reader()`/`app_writer()`/`app_config()`
 /// accessors along with logging macros. Customize as you see fit.
@@ -7,21 +7,18 @@ use abscissa_core::{Command, Options, Runnable};
 use ethers::contract::abigen;
 use ethers::{
     prelude::*,
-    abi::Abi,
     types::{Address},
-    contract::Contract,
     providers::{Provider, Http},
 };
-use std::convert::TryFrom;
-use serde_json;
+use std::{convert::TryFrom, sync::Arc};
 
+//use abigen macro to fetch and incorporate contract ABI
 abigen!(
     MyContract,
     "./contract_abi.json",
     event_derives(serde::Deserialize, serde::Serialize)
 );
 
-///
 /// The `Options` proc macro generates an option parser based on the struct
 /// definition, and is defined in the `gumdrop` crate. See their documentation
 /// for a more comprehensive example:
@@ -33,34 +30,37 @@ pub struct CellarcontractCmd {
     recipient: Vec<String>,
 }
 
+/// impl Runnable for CellarcontractCmd
 impl Runnable for CellarcontractCmd {
     /// Transfer ETH from one account to another with Ganache blockchain emulator.
     fn run(&self) {
         abscissa_tokio::run(&APP, async {
-            // this is a fake address used just for this example
+            // This is a fake address used just for this example
             let address = "0x1dF62f291b2E969fB0849d99D9Ce41e2F137006e".parse::<Address>().unwrap();
             
-            // connect to the network
+            // Connect to the network provider (example below is for Ganache)
             let client = Provider::<Http>::try_from("http://localhost:8545").unwrap();
 
-            let accounts = client.get_accounts().await.unwrap();
+            // MyContract expects Arc, create with client
+            let client = Arc::new(client);
+
+            // Use ethers_rs get_accounts method to get client accounts
+            let _accounts = client.get_accounts().await.unwrap();
+
+            // Create the contract object at the address
+            let contract = MyContract::new(address, client);
             
-            // create the contract object at the address
-            let contract = MyContract::new(address, abi, client);
-            
-            //Test that contract creation was successful by printing contract address
+            // Test that contract creation was successful by printing contract address
             let addr = contract.address();
             //println!("{:?}", addr);
 
-            let get_supply = contract.method::<_, String>("totalSupply", addr).unwrap();
+            // Cellar contract method to get balance of an account
+            let _get_balance = contract.method::<_, Address>("balanceOf", addr).unwrap();
+            // println!("{:?}", get_balance);
+
+            // Cellar contract method to get total supply to an address.
+            let _total_supply = contract.method::<_, String>("totalSupply", ()).unwrap();
             //println!("{:?}", total_supply);
-
-            let reinvest = contract.method::<_, String>("reinvest", ()).unwrap();
-            //println!("{:?}", reinvest);
-
-            let get_name = contract.method::<_, String>("name", ()).unwrap();
-            println!("{:?}", get_name);
-
         })
         .unwrap_or_else(|e| {
             status_err!("executor exited with error: {}", e);
