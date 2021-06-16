9d23a6acfc66da101633afb6b1dda90e7371ce3a
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-06-16 14:08:43 +0100

    adding other structs

--- src/commands/cellarcontract.rs
+++ src/commands/cellarcontract.rs
@@ -35,6 +35,7 @@ impl<T:Middleware > CellarWrapper<T>{
     }
 }
 
+// Struct for CellarTickInfo
 pub struct CellarTickInfo{
     token_id:U256, 
     tick_upper:i32,
@@ -53,6 +54,157 @@ impl CellarTickInfo{
        (self.token_id, self.tick_upper, self.tick_lower, self.weight)
     }
 }
+
+// For struct CellarAddParams
+pub struct CellarAddParams {
+    amount0_desired: U256,
+    amount1_desired: U256,
+    amount0_min: U256,
+    amount1_min: U256,
+    recipient: U256, // since recipient takes in an address, I used the U256 type.
+    deadline: U256,
+}
+
+impl CellarAddParams {
+    pub fn new(amount0_desired: U256, amount1_desired: U256, amount0_min: U256, amount1_min: U256, 
+        recipient: U256, deadline: U256)-> Self{
+        CellarAddParams{amount0_desired, amount1_desired, amount0_min, amount1_min, recipient, deadline}
+    }
+}
+
+// For 
+pub struct CellarRemoveParams {
+    token_amount: U256,
+    amount0_min: U256,
+    amount1_min: U256,
+    recipient: U256,
+    deadline: U256,
+}
+
+impl CellarRemoveParams {
+    pub fn new(token_amount: U256, amount0_min: U256, amount1_min: U256, recipient: U256, deadline: U256,) -> Self{
+        CellarRemoveParams{token_amount, amount0_min, amount1_min, recipient, deadline}
+    }
+}
+
+pub struct PoolInfo {
+    token_0: U256,
+    token_1: U256,
+    fee_level: u32,
+}
+
+impl PoolInfo {
+    pub fn new(token_0: U256, token_1: U256, fee_level: u32,) -> Self {
+        PoolInfo{token_0, token_1, fee_level}
+    }
+}
+
+pub struct UintPair {
+    a: U256,
+    b: U256,
+}
+
+impl UintPair {
+    pub fn new(a: U256, b: U256) -> Self {
+        UintPair{a, b}
+    }
+}
+
+pub struct MintParams {
+    token_0: U256,
+    token_1: U256,
+    fee: u32,
+    tick_lower: i32,
+    tick_upper: i32,
+    amount0_desired: U256,
+    amount1_desired: U256,
+    amount0_min: U256,
+    amount1_min: U256,
+    recipient: U256,
+    deadline: U256,
+}
+
+impl MintParams {
+    pub fn new(token_0: U256, token_1: U256, fee: u32, tick_lower: i32, tick_upper: i32, amount0_desired: U256, 
+        amount1_desired: U256, amount0_min: U256, amount1_min: U256, recipient: U256, deadline: U256,) -> Self {
+            MintParams{token_0, token_1, fee, tick_lower, tick_upper, amount0_desired, amount1_desired, amount0_min, amount1_min, recipient, deadline}
+    }
+}
+
+pub struct MintResult {
+    token_id: U256,
+    liquidity: u128,
+    amount_0: U256,
+    amount_1: U256,
+}
+
+impl MintResult {
+    pub fn new(token_id: U256, liquidity: u128, amount_0: U256, amount_1: U256,) -> Self {
+        MintResult{token_id, liquidity, amount_0, amount_1}
+    }
+}
+
+pub struct IncreaseLiquidityParams {
+    token_id: U256,
+    amount0_desired: U256,
+    amount1_desired: U256,
+    amount0_min: U256,
+    amount1_min: U256,
+    deadline: U256,
+}
+
+impl IncreaseLiquidityParams {
+    pub fn new(token_id: U256, amount0_desired: U256, amount1_desired: U256, amount0_min: U256, amount1_min: U256, deadline: U256,) -> Self {
+        IncreaseLiquidityParams {token_id, amount0_desired, amount1_desired, amount0_min, amount1_min, deadline}
+    }
+}
+
+pub struct DecreaseLiquidityParams {
+    token_id: U256,
+    liquidity: u128,
+    amount0_min: U256,
+    amount1_min: U256,
+    deadline: U256,
+}
+
+impl DecreaseLiquidityParams {
+    pub fn new(token_id: U256, liquidity: u128, amount0_min: U256, amount1_min: U256, deadline: U256) -> Self {
+        DecreaseLiquidityParams{token_id, liquidity, amount0_min, amount1_min, deadline}
+    }
+}
+
+pub struct CollectParams {
+    token_id: U256,
+    recipient: U256,
+    amount0_max: u128,
+    amount1_max: u128,
+}
+
+impl CollectParams {
+    pub fn new(token_id: U256, recipient: U256, amount0_max: u128, amount1_max: u128) -> Self {
+        CollectParams{token_id, recipient, amount0_max, amount1_max}
+    }
+}
+
+pub struct ExactInputSingleParams {
+    token_in: U256,
+    token_out: U256,
+    fee: u32,
+    recipient: U256,
+    deadline: U256,
+    amount_in: U256,
+    amount_out_minimum: U256,
+    sqrt_price_limit_x96: H160, //I'm not so sure, I got a hint from https://docs.rs/primitive-types/0.5.0/primitive_types/struct.H160.html
+}
+
+impl ExactInputSingleParams {
+    pub fn new(token_in: U256, token_out: U256, fee: u32, recipient: U256, deadline: U256, amount_in: U256, amount_out_minimum: U256, sqrt_price_limit_x96: H160, ) -> Self {
+        ExactInputSingleParams{token_in, token_out, fee, recipient, deadline, amount_in, amount_out_minimum, sqrt_price_limit_x96}
+    }
+}
+
+
+
 /// The `Options` proc macro generates an option parser based on the struct
 /// definition, and is defined in the `gumdrop` crate. See their documentation
 /// for a more comprehensive example:
