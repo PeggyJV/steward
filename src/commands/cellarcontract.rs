6f342a9df08a337bd2587e48b7de1617c983415c
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-06-17 10:31:55 +0100

    adding some methods

--- src/commands/cellarcontract.rs
+++ src/commands/cellarcontract.rs
@@ -33,6 +33,22 @@ impl<T:Middleware > CellarWrapper<T>{
     pub async fn rebalance(&mut self, cellar_tick_info:Vec<CellarTickInfo>){
         self.contract.rebalance(cellar_tick_info.into_iter().map(|x|x.to_tuple()).collect());
     }
+
+    pub async fn addLiquidityForUniV3(&mut self, cellar_add_params:Vec<CellarAddParams>){
+        self.contract.addLiquidityForUniV3(cellar_add_params.into_iter().map(|x|x.to_tuple()).collect());
+    }
+
+    pub async fn addLiquidityEthForUniV3(&mut self, cellar_add_params:Vec<CellarAddParams>){
+        self.contract.addLiquidityEthForUniV3(cellar_add_params.into_iter().map(|x|x.to_tuple()).collect());
+    }
+
+    pub async fn removeLiquidityEthFromUniV3(&mut self, cellar_remove_params:Vec<CellarRemoveParams>){
+        self.contract.removeLiquidityEthFromUniV3(cellar_remove_params.into_iter().map(|x|x.to_tuple()).collect());
+    }
+
+    pub async fn removeLiquidityFromUniV(&mut self, cellar_remove_params:Vec<CellarRemoveParams>){
+        self.contract.removeLiquidityFromUniV3(cellar_remove_params.into_iter().map(|x|x.to_tuple()).collect());
+    }
 }
 
 // Struct for CellarTickInfo
@@ -70,9 +86,13 @@ impl CellarAddParams {
         recipient: U256, deadline: U256)-> Self{
         CellarAddParams{amount0_desired, amount1_desired, amount0_min, amount1_min, recipient, deadline}
     }
+
+    pub fn to_tuple(self)->(U256, U256, U256, U256, U256, U256){
+        (self.amount0_desired, self.amount1_desired, self.amount0_min, self.amount1_min, self.recipient, self.deadline)
+     }
 }
 
-// For 
+
 pub struct CellarRemoveParams {
     token_amount: U256,
     amount0_min: U256,
@@ -85,122 +105,10 @@ impl CellarRemoveParams {
     pub fn new(token_amount: U256, amount0_min: U256, amount1_min: U256, recipient: U256, deadline: U256,) -> Self{
         CellarRemoveParams{token_amount, amount0_min, amount1_min, recipient, deadline}
     }
-}
 
-pub struct PoolInfo {
-    token_0: U256,
-    token_1: U256,
-    fee_level: u32,
-}
-
-impl PoolInfo {
-    pub fn new(token_0: U256, token_1: U256, fee_level: u32,) -> Self {
-        PoolInfo{token_0, token_1, fee_level}
-    }
-}
-
-pub struct UintPair {
-    a: U256,
-    b: U256,
-}
-
-impl UintPair {
-    pub fn new(a: U256, b: U256) -> Self {
-        UintPair{a, b}
-    }
-}
-
-pub struct MintParams {
-    token_0: U256,
-    token_1: U256,
-    fee: u32,
-    tick_lower: i32,
-    tick_upper: i32,
-    amount0_desired: U256,
-    amount1_desired: U256,
-    amount0_min: U256,
-    amount1_min: U256,
-    recipient: U256,
-    deadline: U256,
-}
-
-impl MintParams {
-    pub fn new(token_0: U256, token_1: U256, fee: u32, tick_lower: i32, tick_upper: i32, amount0_desired: U256, 
-        amount1_desired: U256, amount0_min: U256, amount1_min: U256, recipient: U256, deadline: U256,) -> Self {
-            MintParams{token_0, token_1, fee, tick_lower, tick_upper, amount0_desired, amount1_desired, amount0_min, amount1_min, recipient, deadline}
-    }
-}
-
-pub struct MintResult {
-    token_id: U256,
-    liquidity: u128,
-    amount_0: U256,
-    amount_1: U256,
-}
-
-impl MintResult {
-    pub fn new(token_id: U256, liquidity: u128, amount_0: U256, amount_1: U256,) -> Self {
-        MintResult{token_id, liquidity, amount_0, amount_1}
-    }
-}
-
-pub struct IncreaseLiquidityParams {
-    token_id: U256,
-    amount0_desired: U256,
-    amount1_desired: U256,
-    amount0_min: U256,
-    amount1_min: U256,
-    deadline: U256,
-}
-
-impl IncreaseLiquidityParams {
-    pub fn new(token_id: U256, amount0_desired: U256, amount1_desired: U256, amount0_min: U256, amount1_min: U256, deadline: U256,) -> Self {
-        IncreaseLiquidityParams {token_id, amount0_desired, amount1_desired, amount0_min, amount1_min, deadline}
-    }
-}
-
-pub struct DecreaseLiquidityParams {
-    token_id: U256,
-    liquidity: u128,
-    amount0_min: U256,
-    amount1_min: U256,
-    deadline: U256,
-}
-
-impl DecreaseLiquidityParams {
-    pub fn new(token_id: U256, liquidity: u128, amount0_min: U256, amount1_min: U256, deadline: U256) -> Self {
-        DecreaseLiquidityParams{token_id, liquidity, amount0_min, amount1_min, deadline}
-    }
-}
-
-pub struct CollectParams {
-    token_id: U256,
-    recipient: U256,
-    amount0_max: u128,
-    amount1_max: u128,
-}
-
-impl CollectParams {
-    pub fn new(token_id: U256, recipient: U256, amount0_max: u128, amount1_max: u128) -> Self {
-        CollectParams{token_id, recipient, amount0_max, amount1_max}
-    }
-}
-
-pub struct ExactInputSingleParams {
-    token_in: U256,
-    token_out: U256,
-    fee: u32,
-    recipient: U256,
-    deadline: U256,
-    amount_in: U256,
-    amount_out_minimum: U256,
-    sqrt_price_limit_x96: H160, //I'm not so sure, I got a hint from https://docs.rs/primitive-types/0.5.0/primitive_types/struct.H160.html
-}
-
-impl ExactInputSingleParams {
-    pub fn new(token_in: U256, token_out: U256, fee: u32, recipient: U256, deadline: U256, amount_in: U256, amount_out_minimum: U256, sqrt_price_limit_x96: H160, ) -> Self {
-        ExactInputSingleParams{token_in, token_out, fee, recipient, deadline, amount_in, amount_out_minimum, sqrt_price_limit_x96}
-    }
+    pub fn to_tuple(self)->(U256, U256, U256, U256, U256){
+        (self.token_amount, self.amount0_min, self.amount1_min, self.recipient, self.deadline)
+     }
 }
 
 
@@ -260,7 +168,7 @@ impl Runnable for CellarcontractCmd {
             // Get balance of Address with cellars `balanceOf` method
             let get_balance = contract.method::<_, U256>("balanceOf", addr).unwrap();
             let balance: U256 = get_balance.call().await.unwrap();
-            println!("{:?}", balance); 
+            println!("{:?}", balance);
             
             //make transaction.
             let make_transfer = contract.transfer_from( from, to, value).from(from);
