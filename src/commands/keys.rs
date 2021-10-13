2d3c6d134bc72fb6b037413cd7cb5a3725260bf9
hannydevelop <ukpaiugochiibem@gmail.com>
2021-10-13 09:29:32 +0000

    keys commands ready

--- src/commands/keys.rs
+++ src/commands/keys.rs
@@ -4,22 +4,17 @@ mod import;
 mod list;
 mod show;
 
-use abscissa_core::{Command, Options, Runnable};
+use abscissa_core::{Command, Clap, Runnable};
 
-#[derive(Command, Debug, Options, Runnable)]
+#[derive(Command, Debug, Clap, Runnable)]
 pub enum KeysCmd {
-    #[options(help = "add [name] (password)")]
     Add(add::AddKeyCmd),
 
-    #[options(help = "show [name]")]
     Show(show::ShowKeyCmd),
 
-    #[options(help = "delete [name]")]
     Delete(delete::DeleteKeyCmd),
 
-    #[options(help = "list")]
     List(list::ListKeyCmd),
 
-    #[options(help = "import")]
     Import(import::ImportEthKeyCmd),
 }
--- src/commands/keys/list.rs
+++ src/commands/keys/list.rs
@@ -1,9 +1,9 @@
 use super::show::ShowKeyCmd;
 use crate::application::APP;
-use abscissa_core::{Application, Command, Options, Runnable};
+use abscissa_core::{Application, Command, Clap, Runnable};
 use std::path::Path;
 
-#[derive(Command, Debug, Default, Options)]
+#[derive(Command, Debug, Default, Clap)]
 pub struct ListKeyCmd {}
 
 // Entry point for `keys list`
