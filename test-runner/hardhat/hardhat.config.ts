1a99f122e416e60977f3ad2c2ad250e6f0393d9a
hannydevelop <ukpaiugochiibem@gmail.com>
2021-10-20 16:38:06 +0000

    spining cellars CPST successful

--- test-runner/hardhat/hardhat.config.ts
+++ test-runner/hardhat/hardhat.config.ts
@@ -1,39 +1,58 @@
 import "@nomiclabs/hardhat-waffle";
+import { task } from 'hardhat/config';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();for (const account of accounts) {
-      console.log(account.address);
-    }
-  });// You need to export an object to set up your config
-  // Go to https://hardhat.org/config/ to learn more
+// Simple hardhat task for Cellars Rebalancer test
+
+task("integration_test_setup", 'Sets up contracts for the integration test', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  const ADDRESSES = {
+    CELLAR_OWNER: '0xB6C951cf962977f123bF37de42945f7ca1cd2A52',
+    CELLAR: '0x6ea5992aB4A78D5720bD12A089D13c073d04B55d',
+  };
+
+  console.log('retrieving cellar contract');
+  const cellarSigner = await hre.ethers.getSigner(ADDRESSES.CELLAR_OWNER)
+
+  // get Cellars contract from forked mainnet
+  const Cellar = hre.ethers.getContractAt(
+    'CellarPoolShare',
+    ADDRESSES.CELLAR,
+    cellarSigner,
+  );
+  const cellar = await Cellar;
+  console.log(cellar)
+
+});
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
 
 module.exports = {
-    networks: {
-      hardhat: {
-        forking: {
-          url: "https://mainnet.infura.io/v3/d6f22be0f7fd447186086d2495779003",
-          blockNumber: 13103326
-        }
-      },
-    },
-    solidity: {
-      version: "0.7.3",
-      settings: {
-        optimizer: {
-          enabled: true
-        }
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://mainnet.infura.io/v3/d6f22be0f7fd447186086d2495779003",
+        blockNumber: 13103326
       }
     },
-    // TODO: add forking configuration
-    typechain: {
-      outDir: "typechain",
-      target: "ethers-v5",
-      runOnCompile: true
-    },
-    gasReporter: {
-      enabled: true
-    },
-  };
-  
+  },
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+  // TODO: add forking configuration
+  typechain: {
+    outDir: "typechain",
+    target: "ethers-v5",
+    runOnCompile: true
+  },
+  gasReporter: {
+    enabled: true
+  },
+};
