b7472f2a99f886b69484f2252d2c7fd118e30970
hannydevelop <ukpaiugochiibem@gmail.com>
2021-11-24 07:23:38 +0000

    successfully generated abi for uniswap router

--- abi_build/src/main.rs
+++ abi_build/src/main.rs
@@ -56,6 +56,30 @@ fn main() {
     }
 
     // Uniswap Router
+    let abigen = match Abigen::new("Erc20", "../rebalancer_abi/uniswap_router_abi.json") {
+        Ok(abigen) => abigen,
+        Err(e) => {
+            println!("Could not open uniswap_router_abi.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let abi = match abigen
+        .add_event_derive("serde::Deserialize")
+        .add_event_derive("serde::Serialize")
+        .generate()
+    {
+        Ok(abi) => abi,
+        Err(e) => {
+            println!("Could not generate abi from uniswap_router_abi.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match abi.write_to_file("../rebalancer_abi/src/uniswap_router.rs") {
+        Ok(_) => (),
+        Err(e) => println!("Error writing uniswap_router.rs: {}", e),
+    }
 
     // Uniswapv3 Pool
 
