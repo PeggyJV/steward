7597b7321a17e88e5d484859fb79e801f4da4681
hannydevelop <ukpaiugochiibem@gmail.com>
2021-11-24 07:09:15 +0000

    successfully created erc20 bindings

--- abi_build/src/main.rs
+++ abi_build/src/main.rs
@@ -30,6 +30,31 @@ fn main() {
 
     // Erc20
 
+    let abigen = match Abigen::new("Erc20", "../rebalancer_abi/erc20_abi.json") {
+        Ok(abigen) => abigen,
+        Err(e) => {
+            println!("Could not open erc20_abi.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let abi = match abigen
+        .add_event_derive("serde::Deserialize")
+        .add_event_derive("serde::Serialize")
+        .generate()
+    {
+        Ok(abi) => abi,
+        Err(e) => {
+            println!("Could not generate abi from erc20.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match abi.write_to_file("../rebalancer_abi/src/erc20.rs") {
+        Ok(_) => (),
+        Err(e) => println!("Error writing erc20.rs: {}", e),
+    }
+
     // Uniswap Router
 
     // Uniswapv3 Pool
--- rebalancer_abi/src/lib.rs
+++ rebalancer_abi/src/lib.rs
@@ -1,1 +1,2 @@
 mod aave;
+mod erc20;
