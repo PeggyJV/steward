46c9c13bb90b37e75e9a01698f57b33d818fd81a
hannydevelop <ukpaiugochiibem@gmail.com>
2021-11-24 07:07:31 +0000

    successfully created aave bindings

--- abi_build/src/main.rs
+++ abi_build/src/main.rs
@@ -2,7 +2,31 @@ use ethers::contract::Abigen;
 use std::process;
 
 fn main() {
-    // Aave
+    // Aave    
+    let abigen = match Abigen::new("Aave", "../rebalancer_abi/aave_lending_abi.json") {
+        Ok(abigen) => abigen,
+        Err(e) => {
+            println!("Could not open aave_lending_abi.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    let abi = match abigen
+        .add_event_derive("serde::Deserialize")
+        .add_event_derive("serde::Serialize")
+        .generate()
+    {
+        Ok(abi) => abi,
+        Err(e) => {
+            println!("Could not generate abi from aave_lending_abi.json: {}", e);
+            process::exit(1);
+        }
+    };
+
+    match abi.write_to_file("../rebalancer_abi/src/aave.rs") {
+        Ok(_) => (),
+        Err(e) => println!("Error writing aave.rs: {}", e),
+    }
 
     // Erc20
 
@@ -12,3 +36,4 @@ fn main() {
 
     // Weth
 }
+
