fc27e547597e2794c5f4f6af9f31ab0665b27250
hannydevelop <ukpaiugochiibem@gmail.com>
2021-12-02 18:40:15 +0000

    added cosmos keys module

--- cellar_rebalancer/src/commands/keys.rs
+++ cellar_rebalancer/src/commands/keys.rs
@@ -1,10 +1,15 @@
 mod eth;
+mod cosmos;
 
 use abscissa_core::{Command, Clap, Runnable};
 use crate::commands::keys::eth::EthKeysCmd;
+use crate::commands::keys::cosmos::CosmosKeysCmd;
 
 #[derive(Command, Debug, Clap, Runnable)]
 pub enum KeysCmd {
     #[clap(subcommand)]
     EthKeysCmd(EthKeysCmd),
+
+    #[clap(subcommand)]
+    CosmosKeysCmd(CosmosKeysCmd),
 }
