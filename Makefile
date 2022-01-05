13ccb98540c21df4de7e4042dd7746e49c9d495d
Eric Bolten <eric@bolten.dev>
2022-01-04 17:13:47 -0800

    somm rebalance tests, with unused steward images

---
+++ Makefile
@@ -0,0 +1,54 @@
+.DEFAULT_GOAL := e2e_rebalance
+
+VALIDATOR_IMAGE := "ghcr.io/peggyjv/sommelier-sommelier:v3.0.0-pre2"
+ORCHESTRATOR_IMAGE := "ghcr.io/peggyjv/gravity-bridge-orchestrator:v0.3.0"
+
+e2e_build_images:
+	@docker pull $(VALIDATOR_IMAGE)
+	@docker tag $(VALIDATOR_IMAGE) sommelier:prebuilt
+	@docker pull $(ORCHESTRATOR_IMAGE)
+	@docker tag $(ORCHESTRATOR_IMAGE) orchestrator:prebuilt
+	@docker build -t steward:prebuilt -f Dockerfile .
+	@docker build -t ethereum:prebuilt -f integration_tests/ethereum/Dockerfile integration_tests/ethereum/
+
+e2e_clean_slate:
+	@docker rm --force \
+		$(shell docker ps -qa --filter="name=ethereum") \
+		$(shell docker ps -qa --filter="name=sommelier") \
+		$(shell docker ps -qa --filter="name=orchestrator") \
+		$(shell docker ps -qa --filter="name=steward") \
+		1>/dev/null \
+		2>/dev/null \
+		|| true
+	@docker wait \
+		$(shell docker ps -qa --filter="name=ethereum") \
+		$(shell docker ps -qa --filter="name=sommelier") \
+		$(shell docker ps -qa --filter="name=orchestrator") \
+		$(shell docker ps -qa --filter="name=steward") \
+		1>/dev/null \
+		2>/dev/null \
+		|| true
+	@docker network prune --force 1>/dev/null 2>/dev/null || true
+	@cd integration_tests && go test -c
+
+e2e_rebalance: e2e_clean_slate
+	@E2E_SKIP_CLEANUP=true integration_tests/integration_tests.test -test.failfast -test.v -test.run IntegrationTestSuite -testify.m TestRebalance || make -s fail
+
+fail:
+	@echo 'test failed; dumping container logs into ./testdata for review'
+	@mkdir -p ./testdata
+	@docker logs ethereum > testlogs/ethereum.log 2>&1 || true
+	@docker logs sommelier0 > testdata/sommelier0.log 2>&1 || true
+	@docker logs sommelier1 > testdata/sommelier1.log 2>&1 || true
+	@docker logs sommelier2 > testdata/sommelier2.log 2>&1 || true
+	@docker logs sommelier3 > testdata/sommelier3.log 2>&1 || true
+	@docker logs orchestrator0 > testdata/orchestrator0.log 2>&1 || true
+	@docker logs orchestrator1 > testdata/orchestrator1.log 2>&1 || true
+	@docker logs orchestrator2 > testdata/orchestrator2.log 2>&1 || true
+	@docker logs orchestrator3 > testdata/orchestrator3.log 2>&1 || true
+	@docker logs steward0 > testdata/steward0.log 2>&1 || true
+	@docker logs steward1 > testdata/steward1.log 2>&1 || true
+	@docker logs steward2 > testdata/steward2.log 2>&1 || true
+	@docker logs steward3 > testdata/steward3.log 2>&1 || true
+	@false
+
---
+++ integration_tests/ethereum/hardhat.config.ts
@@ -0,0 +1,122 @@
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-waffle';
+import {task} from "hardhat/config";
+import * as constants from "./addresses";
+
+task(
+    'integration_test_setup',
+    'Sets up contracts for the integration test',
+    async (args, hre) => {
+
+        // Take over vitalik.eth
+        await hre.network.provider.request({
+            method: 'hardhat_impersonateAccount',
+            params: [constants.WHALE],
+        });
+
+        // Send ETH to needed parties
+        const whaleSigner = await hre.ethers.getSigner(constants.WHALE);
+
+        for (let addr of constants.VALIDATORS) {
+            await whaleSigner.sendTransaction({
+                to: addr,
+                value: hre.ethers.utils.parseEther('100'),
+            });
+        }
+
+        let powers: number[] = [1073741823,1073741823,1073741823,1073741823];
+        let powerThreshold: number = 6666;
+
+        const Gravity = await hre.ethers.getContractFactory("Gravity");
+        const gravity = (await Gravity.deploy(
+            hre.ethers.utils.formatBytes32String("gravitytest"),
+            powerThreshold,
+            constants.VALIDATORS,
+            powers
+        ));
+
+        await gravity.deployed();
+        console.log(`gravity contract deployed at - ${gravity.address}`)
+
+        const Cellar = await hre.ethers.getContractFactory("CellarPoolShare");
+        const cellar = (await Cellar.deploy(
+            "mock cellar",
+            "mock",
+            "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+            "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+            3000,
+            [[0, 600, 300, 900]],
+        ));
+        await cellar.deployed();
+        console.log(`cellar contract deploy at - ${cellar.address}`);
+
+        let cellarSignerAddress = await cellar.signer.getAddress()
+        await hre.network.provider.request({
+            method: 'hardhat_impersonateAccount',
+            params: [cellarSignerAddress],
+        });
+
+        let { adjusterHash } = await cellar.setAdjuster(gravity.address, true, {
+            gasPrice: hre.ethers.BigNumber.from('99916001694'),
+        })
+        console.log(
+            `Cellar contract at ${cellar.address} now has adjuster Gravity contract ${gravity.address} with hash ${adjusterHash}`,
+        )
+        let { hash } = await cellar.transferOwnership(gravity.address, {
+            gasPrice: hre.ethers.BigNumber.from('99916001694'),
+            from: cellarSignerAddress
+        });
+        console.log(
+            `Cellar contract at ${cellar.address} is now owned by Gravity contract at ${gravity.address} with hash ${hash}`,
+        );
+
+        await hre.network.provider.send("evm_setIntervalMining", [1000]);
+
+        await hre.run('node');
+    });
+
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const ARCHIVE_NODE_URL = process.env.ARCHIVE_NODE_URL;
+
+module.exports = {
+    networks: {
+        hardhat: {
+            forking: {
+                url: ARCHIVE_NODE_URL,
+                blockNumber: 13405367,
+            },
+        },
+    },
+    solidity: {
+        compilers: [
+            {
+                version: '0.7.6',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+            {
+                version: '0.8.10',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                    },
+                },
+            },
+        ],
+    },
+    typechain: {
+        outDir: 'typechain',
+        target: 'ethers-v5',
+        runOnCompile: true,
+    },
+    gasReporter: {
+        enabled: true,
+    },
+};
