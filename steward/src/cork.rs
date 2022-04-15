2e5aaf7f4094e5e452bb3a1089e7633e1761ee5f
hannydevelop <ukpaiugochiibem@gmail.com>
2022-04-15 08:35:48 +0000

    making progress

--- steward/src/cork.rs
+++ steward/src/cork.rs
@@ -4,15 +4,22 @@ use crate::{
     error::{Error, ErrorKind},
     prelude::APP,
     somm_send,
+    cork::abi::ParamType::String as abi_type,
 };
 use abscissa_core::{
     tracing::log::{debug, error, warn},
     Application,
 };
 use deep_space::{Coin, Contact};
-use gravity_bridge::gravity_proto::cosmos_sdk_proto::cosmos::base::abci::v1beta1::TxResponse;
+use ethers::prelude::*;
+use gravity_bridge::{
+    gravity_proto::cosmos_sdk_proto::cosmos::base::abci::v1beta1::TxResponse,
+    gravity_utils::ethereum::downcast_to_u64,
+};
+use prost::Message;
+use signatory::FsKeyStore;
 use somm_proto::cork::{query_client::QueryClient as CorkQueryClient, Cork, QueryCellarIDsRequest};
-use std::time::Duration;
+use std::{convert::TryFrom, path, time::Duration};
 use steward_proto::{
     self,
     steward::{
@@ -103,6 +110,66 @@ impl steward::contract_call_server::ContractCall for DirectCorkHandler {
     ) -> Result<Response<SubmitResponse>, Status> {
         debug!("received contract call request");
         let request = request.get_ref();
+        let config = APP.config();
+
+        let keystore = path::Path::new(&config.keystore);
+        let keystore = FsKeyStore::create_or_open(keystore).expect("Could not open keystore");
+
+        let name = &config
+            .keys
+            .rebalancer_key
+            .parse()
+            .expect("Could not parse name");
+        let key = keystore.load(name).expect("Could not load key");
+
+        let key = key
+            .to_pem()
+            .parse::<k256::elliptic_curve::SecretKey<k256::Secp256k1>>()
+            .expect("Could not parse key");
+
+        let wallet: LocalWallet = Wallet::from(key);
+
+        let eth_host = config.ethereum.rpc.clone();
+        let provider = Provider::<Http>::try_from(eth_host.clone())
+            .unwrap_or_else(|_| panic!("could not get provider from eth_host {}", eth_host));
+        let chain_id = provider
+            .get_chainid()
+            .await
+            .expect("Could not retrieve chain ID");
+
+        let chain_id =
+            downcast_to_u64(chain_id).expect("Chain ID overflowed when downcasting to u64");
+        let client = SignerMiddleware::new(provider, wallet.clone().with_chain_id(chain_id));
+
+        let cellar_address = request.cellar_id.clone();
+
+        if let Err(err) = cellars::validate_cellar_id(&cellar_address) {
+            return Err(tonic::Status::invalid_argument(err));
+        }
+
+        let address: H160 = cellar_address.parse().expect("could not parse cellar address");
+
+        let contract_call_data = match request.call_data.clone() {
+            Some(call) => call,
+            None => return Err(tonic::Status::invalid_argument("err")),
+        };
+        let encoded_call = get_encoded_call(contract_call_data).unwrap();
+
+        // (ugly way to write the ABI inline, you can otherwise read it from a file)
+        // let abi: abi::Abi = serde_json::from_str(r#"[{"inputs":[{"internalType":"string","name":"value","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"author","type":"address"},{"indexed":true,"internalType":"address","name":"oldAuthor","type":"address"},{"indexed":false,"internalType":"string","name":"oldValue","type":"string"},{"indexed":false,"internalType":"string","name":"newValue","type":"string"}],"name":"ValueChanged","type":"event"},{"inputs":[],"name":"getValue","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastSender","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"value","type":"string"}],"name":"setValue","outputs":[],"stateMutability":"nonpayable","type":"function"}]"#).unwrap();
+
+        let abi = abi::decode(&[abi_type], &encoded_call).unwrap();
+
+        let abi = abi[0].to_string();
+
+        let abi: abi::Abi = serde_json::from_str(&abi).unwrap();
+        
+        // create the contract object at the address
+        let contract = Contract::new(address, abi, &client);
+
+        // Calling constant methods is done by calling `call()` on the method builder.
+        // (if the function takes no arguments, then you must use `()` as the argument)
+        let init_value: String = contract.method::<_, String>("getValue", ()).unwrap().call().await.unwrap();
 
         Ok(Response::new(SubmitResponse {}))
     }
