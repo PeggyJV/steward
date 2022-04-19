196ea59e52a490963f94179e9cc8760d6c7693b5
hannydevelop <ukpaiugochiibem@gmail.com>
2022-04-19 16:47:12 +0000

    errors

--- steward/src/cork.rs
+++ steward/src/cork.rs
@@ -1,7 +1,6 @@
 use crate::{
     cellars::{self, aave_v2_stablecoin},
     config,
-    cork::abi::ParamType::String as abi_type,
     error::{Error, ErrorKind},
     prelude::APP,
     somm_send,
@@ -18,7 +17,9 @@ use gravity_bridge::{
 };
 use signatory::FsKeyStore;
 use somm_proto::cork::{query_client::QueryClient as CorkQueryClient, Cork, QueryCellarIDsRequest};
-use std::{convert::TryFrom, path, time::Duration};
+use std::{convert::TryFrom, path, sync::Arc, time::Duration};
+use steward_abi::aave_v2_stablecoin::AaveV2StablecoinCellar;
+use steward_proto::steward::aave_v2_stablecoin::Function;
 use steward_proto::{
     self,
     steward::{
@@ -109,25 +110,32 @@ impl steward::contract_call_server::ContractCall for DirectCorkHandler {
     ) -> Result<Response<SubmitResponse>, Status> {
         debug!("received contract call request");
         let request = request.get_ref();
-        let config = APP.config();
 
+        
+        let contract_call_data = match request.call_data.clone() {
+            Some(call) => call,
+            None => return Err(tonic::Status::invalid_argument("err")),
+        };
+    
+        let config = APP.config();
+    
         let keystore = path::Path::new(&config.keystore);
         let keystore = FsKeyStore::create_or_open(keystore).expect("Could not open keystore");
-
+    
         let name = &config
             .keys
             .rebalancer_key
             .parse()
             .expect("Could not parse name");
         let key = keystore.load(name).expect("Could not load key");
-
+    
         let key = key
             .to_pem()
             .parse::<k256::elliptic_curve::SecretKey<k256::Secp256k1>>()
             .expect("Could not parse key");
-
+    
         let wallet: LocalWallet = Wallet::from(key);
-
+    
         let eth_host = config.ethereum.rpc.clone();
         let provider = Provider::<Http>::try_from(eth_host.clone())
             .unwrap_or_else(|_| panic!("could not get provider from eth_host {}", eth_host));
@@ -135,48 +143,59 @@ impl steward::contract_call_server::ContractCall for DirectCorkHandler {
             .get_chainid()
             .await
             .expect("Could not retrieve chain ID");
-
-        let chain_id =
-            downcast_to_u64(chain_id).expect("Chain ID overflowed when downcasting to u64");
+    
+        let chain_id = downcast_to_u64(chain_id).expect("Chain ID overflowed when downcasting to u64");
         let client = SignerMiddleware::new(provider, wallet.clone().with_chain_id(chain_id));
-
+    
         let cellar_address = request.cellar_id.clone();
-
+    
         if let Err(err) = cellars::validate_cellar_id(&cellar_address) {
             return Err(tonic::Status::invalid_argument(err));
         }
-
-        let address: H160 = cellar_address
+    
+        let address: Address = cellar_address
             .parse()
             .expect("could not parse cellar address");
-
-        let contract_call_data = match request.call_data.clone() {
-            Some(call) => call,
-            None => return Err(tonic::Status::invalid_argument("err")),
-        };
-        let encoded_call = get_encoded_call(contract_call_data).unwrap();
-
-        // (ugly way to write the ABI inline, you can otherwise read it from a file)
-        // let abi: abi::Abi = serde_json::from_str(r#"[{"inputs":[{"internalType":"string","name":"value","type":"string"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"author","type":"address"},{"indexed":true,"internalType":"address","name":"oldAuthor","type":"address"},{"indexed":false,"internalType":"string","name":"oldValue","type":"string"},{"indexed":false,"internalType":"string","name":"newValue","type":"string"}],"name":"ValueChanged","type":"event"},{"inputs":[],"name":"getValue","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"lastSender","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"value","type":"string"}],"name":"setValue","outputs":[],"stateMutability":"nonpayable","type":"function"}]"#).unwrap();
-
-        let abi = abi::decode(&[abi_type], &encoded_call).unwrap();
-
-        for abi in abi {
-            let abi = abi.to_string();
-
-            let abi: abi::Abi = serde_json::from_str(&abi).unwrap();
-
-            // create the contract object at the address
-            let contract = Contract::new(address, abi, &client);
-
-            // Calling constant methods is done by calling `call()` on the method builder.
-            // (if the function takes no arguments, then you must use `()` as the argument)
-            contract
-                .method::<_, String>("getValue", ())
-                .unwrap()
-                .call()
-                .await
-                .unwrap();
+    
+        match contract_call_data {
+            AaveV2Stablecoin(call) => {
+                let contract = AaveV2StablecoinCellar::new(address, Arc::new(client));
+    
+                let call_functions = match call.function.unwrap() {
+                    Function::AccruePlatformFees(_) => contract.accrue_platform_fees(),
+                    Function::ClaimAndUnstake(_) => contract.claim_and_unstake(),
+                    Function::ClaimAndUnstakeAmount(params) => {
+                        contract.claim_and_unstake_with_amount(params.amount.into())
+                    }
+                    Function::EnterStrategy(_) => contract.enter_strategy(),
+                    Function::Rebalance(params) => contract.rebalance(
+                        params
+                            .address
+                            .parse::<H160>()
+                            .expect("failed to parse token address"),
+                        params.min_new_lending_token_amount.into(),
+                    ),
+                    Function::Reinvest(params) => contract.reinvest(params.min_assets_out.into()),
+                    Function::ReinvestAmount(params) => contract
+                        .reinvest_with_amount(params.amount.into(), params.min_assets_out.into()),
+                    Function::RemoveLiquidityRestriction(_) => contract.remove_liquidity_restriction(),
+                    Function::SetInputToken(params) => contract.set_input_token(
+                        params
+                            .address
+                            .parse::<H160>()
+                            .expect("failed to parse token address"),
+                        params.is_approved.into(),
+                    ),
+                    Function::Sweep(params) => contract.sweep(
+                        params
+                            .address
+                            .parse::<H160>()
+                            .expect("failed to parse token address"),
+                    ),
+                    Function::TransferFees(_) => contract.transfer_fees(),
+                };
+                call_functions.send();
+            }
         }
 
         Ok(Response::new(SubmitResponse {}))
