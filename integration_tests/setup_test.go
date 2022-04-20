95aa374e61bc85505da267f1759f8c2555076583
hannydevelop <ukpaiugochiibem@gmail.com>
2022-04-20 14:07:32 +0000

    little tweak

--- integration_tests/setup_test.go
+++ integration_tests/setup_test.go
@@ -710,10 +710,9 @@ func (s *IntegrationTestSuite) runStewards() {
 	s.T().Log("starting steward containers...")
 	s.stewResources = make([]*dockertest.Resource, len(s.chain.stewards))
 	for i, steward := range s.chain.stewards {
-		stewardCfg := fmt.Sprintf(`[cosmos]
+		stewardCfg := fmt.Sprintf(`keystore = "/tmp/keystore"
+[cosmos]
 grpc = "http://%s:9090"
-
-keystore = "/tmp/keystore"
 
 [cosmos.gas_price]
 amount = 1000000000
