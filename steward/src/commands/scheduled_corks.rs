575334b2e91e0b2b4a0d0ab3290a34a43da6ab61
hannydevelop <ukpaiugochiibem@gmail.com>
2022-04-27 18:00:25 +0000

    minute changes

--- steward/src/commands/scheduled_corks.rs
+++ steward/src/commands/scheduled_corks.rs
@@ -1,9 +1,17 @@
 use abscissa_core::{clap::Parser, Command, Runnable};
+use ethers::abi::Function;
 
 /// Scheduled corks subcommand
 #[derive(Command, Debug, Parser)]
-pub struct ScheduledCorksCmd {}
+pub struct ScheduledCorksCmd {
+    cellar_id: String,
+    block_height: u64,
+    function: String,
+}
 
 impl Runnable for ScheduledCorksCmd {
-    fn run(&self) {}
+    fn run(&self) {
+        let address = self.cellar_id.clone();
+        let function: Function = self.function.clone().parse().unwrap;
+    }
 }
