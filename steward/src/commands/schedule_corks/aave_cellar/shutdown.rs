2a59e3132c1dd352092166ea8b2d207b2cdd3679
hannydevelop <ukpaiugochiibem@gmail.com>
2022-05-06 09:55:40 +0000

    fixed reviews

--- steward/src/commands/schedule_corks/aave_cellar/shutdown.rs
+++ steward/src/commands/schedule_corks/aave_cellar/shutdown.rs
@@ -17,7 +17,7 @@ const CHAIN_PREFIX: &str = "somm";
 /// Shutdown subcommand
 #[derive(Command, Debug, Parser)]
 #[clap(
-    long_about = "DESCRIPTION \n\n Shutdown target Cellar.\n This command schedules the shutdown of a Cellar. This is a validator only \n command and can only be run by validators. It Schedules shutdown based on the height specified by \n the validators. Therefore, it'll execute the function when the chain reaches that height."
+    long_about = "DESCRIPTION \n\n Shutdown target Cellar.\n This command schedules the shutdown of a Cellar. This is a validator only \n command and can only be run by validators. It Schedules shutdown based on the height specified by \n the validators. Therefore, it'll execute the function when the chain reaches that height. This command also takes the shutdown and exit position flag."
 )]
 pub struct ShutdownCmd {
     /// Target contract for scheduled cork.
@@ -27,6 +27,14 @@ pub struct ShutdownCmd {
     /// Block height to schedule cork.
     #[clap(short = 'b', long)]
     height: u64,
+
+    ///Set to true if you want to shutdown Aave Cellar.
+    #[clap(short = 's', long)]
+    shut_down: bool,
+
+    /// Set to true if you want to exit current position.
+    #[clap(short = 'e', long)]
+    exit_position: bool,
 }
 
 impl Runnable for ShutdownCmd {
@@ -35,13 +43,13 @@ impl Runnable for ShutdownCmd {
 
         abscissa_tokio::run_with_actix(&APP, async {
             let call = SetShutdownCall {
-                shutdown: true,
-                exit_position: true,
+                shutdown: self.shut_down,
+                exit_position: self.exit_position,
             };
             let encoded_call = AaveV2StablecoinCellarCalls::SetShutdown(call).encode();
 
             cellars::validate_cellar_id(self.contract.as_str()).unwrap_or_else(|err| {
-                status_err!("Can't validate cellar ID: {}", err);
+                status_err!("Can't validate contract address format: {}", err);
                 std::process::exit(1);
             });
 
@@ -68,7 +76,9 @@ impl Runnable for ShutdownCmd {
                 self.height,
             )
             .await
-            .expect("Error, can't schedule shutdown");
+            .unwrap_or_else(|err| {
+                panic!("{:?}", err);
+            })
         })
         .unwrap_or_else(|e| {
             status_err!("executor exited with error: {}", e);
