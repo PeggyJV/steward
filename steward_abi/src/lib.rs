830065c43fb3d0eb7d706bbfd0fa07afe0139949
Collin Brittain <collinbrittain@gmail.com>
2022-07-15 11:17:33 -0500

    Clippy

--- steward_abi/src/lib.rs
+++ steward_abi/src/lib.rs
@@ -1,4 +1,4 @@
 // Clippy wants enum variants that are significantly large to be in a Box<T>.
 // Since this is generated code we just ignore this rule.
-#[allow(clippy::large_enum_variant)]
+#[allow(clippy::large_enum_variant, clippy::extra_unused_lifetimes)]
 pub mod aave_v2_stablecoin;
