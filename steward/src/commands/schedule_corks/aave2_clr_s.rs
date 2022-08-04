587221965ce7781e9ef984049ebc4591d99e89fa
hannydevelop <ukpaiugochiibem@gmail.com>
2022-08-04 06:36:49 +0000

    moved commands to aave2_clr_s.rs folder

--- steward/src/commands/schedule_corks/aave2_clr_s.rs
+++ steward/src/commands/schedule_corks/aave2_clr_s.rs
@@ -1,5 +1,9 @@
 mod initiate_shutdown;
 mod lift_shutdown;
+mod fees_distributor;
+mod sweep;
+mod trust;
+mod transfer_ownership;
 
 use abscissa_core::{clap::Parser, Command, Runnable};
 
@@ -8,4 +12,8 @@ use abscissa_core::{clap::Parser, Command, Runnable};
 pub enum AaveV2StablecoinCellarCmd {
     InitiateShutdown(initiate_shutdown::InitiateShutdownCmd),
     LiftShutdown(lift_shutdown::LiftShutdownCmd),
+    FeesDistributor(fees_distributor::FeesDistributorCmd),
+    Sweep(sweep::SweepCmd),
+    Trust(trust::TrustCmd),
+    TransferOwnership(transfer_ownership::TransferOwnershipCmd),
 }
