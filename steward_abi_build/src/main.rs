b47423c283baa90251d43c01ee1c7d9797ea4758
Collin Brittain <collinbrittain@gmail.com>
2022-09-08 09:28:06 -0500

    Add Cellar.sol ABI definitions

--- steward_abi_build/src/main.rs
+++ steward_abi_build/src/main.rs
@@ -31,4 +31,35 @@ fn main() {
         Ok(_) => (),
         Err(e) => println!("Error writing aave_v2_stablecoin.rs: {}", e),
     }
+
+
+    // Cellar.sol
+    let cellar_contract = "Cellar";
+    let abigen = match Abigen::new(
+        cellar_contract,
+        format!("../steward_abi/{}.json", cellar_contract),
+    ) {
+        Ok(abigen) => abigen,
+        Err(e) => {
+            println!("Could not open {}.json: {}", cellar_contract, e);
+            process::exit(1);
+        }
+    };
+
+    let abi = match abigen
+        .add_event_derive("serde::Deserialize")
+        .add_event_derive("serde::Serialize")
+        .generate()
+    {
+        Ok(abi) => abi,
+        Err(e) => {
+            println!("Could not generate abi from {}.json: {}", cellar_contract, e);
+            process::exit(1);
+        }
+    };
+
+    match abi.write_to_file("../steward_abi/src/cellar.rs") {
+        Ok(_) => (),
+        Err(e) => println!("Error writing cellar.rs: {}", e),
+    }
 }
