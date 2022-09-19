64fc05daf7ed05fab5500dd669e264b843e0ed38
Collin Brittain <collinbrittain@gmail.com>
2022-09-19 08:23:22 -0500

    Initial mock of Cellar.sol

---
+++ integration_tests/ethereum/contracts/MockCellar.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity 0.8.16;
+
+/**
+ * @title Sommelier Cellar
+ * @notice A composable ERC4626 that can use a set of other ERC4626 or ERC20 positions to earn yield.
+ * @author Brian Le
+ */
+contract Cellar {
+    constructor() {}
+
+    // =========================================== POSITION LOGIC ===========================================
+
+    /**
+     * @notice Emitted on rebalancing positions.
+     * @param fromPosition the address of the position rebalanced from
+     * @param toPosition the address of the position rebalanced to
+     * @param assetsFrom the amount of assets withdrawn from the position rebalanced from
+     */
+    event Rebalance(address indexed fromPosition, address indexed toPosition, uint256 assetsFrom);
+
+    enum Exchange {
+        UNIV2,
+        UNIV3
+    }
+
+    /**
+     * @notice Move assets between positions. To move assets from/to this cellar's holdings, specify
+     *         the address of this cellar as the `fromPosition`/`toPosition`.
+     * @param fromPosition address of the position to move assets from
+     * @param toPosition address of the position to move assets to
+     * @param assetsFrom amount of assets to move from the from position
+     */
+    function rebalance(
+        address fromPosition,
+        address toPosition,
+        uint256 assetsFrom,
+        Exchange exchange,
+        bytes calldata params
+    ) external returns (uint256 assetsTo) {
+        if (exchange == Exchange.UNIV2) {
+            (address[] memory path, uint256 amount, uint256 amountOutMin) = abi.decode(
+                params,
+                (address[], uint256, uint256)
+            );
+        }
+        if (exchange == Exchange.UNIV3) {
+            (address[] memory path, uint24[] memory poolFees, uint256 amount, uint256 amountOutMin) = abi.decode(
+                params,
+                (address[], uint24[], uint256, uint256)
+            );
+        }
+        emit Rebalance(fromPosition, toPosition, assetsFrom);
+    }
+}
