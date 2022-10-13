f2d8b2406399bc2191ac1af6554841b7adfe892b
hannydevelop <ukpaiugochiibem@gmail.com>
2022-10-13 12:49:23 +0100

    Edited cork.rs, removed traces of cork from cork.rs

--- steward/src/cork.rs
+++ steward/src/cork.rs
@@ -1,58 +1,20 @@
 use crate::{
-    cellars::{self, aave_v2_stablecoin},
     config,
-    error::{Error, ErrorKind},
+    error::{Error},
     prelude::APP,
     somm_send,
 };
 use abscissa_core::{
-    tracing::log::{debug, error, info, warn},
+    tracing::log::{debug},
     Application,
 };
 use deep_space::{Coin, Contact};
 use gravity_bridge::gravity_proto::cosmos_sdk_proto::cosmos::base::abci::v1beta1::TxResponse;
-use somm_proto::cork::{query_client::QueryClient as CorkQueryClient, Cork, QueryCellarIDsRequest};
+use somm_proto::cork::{Cork};
 use std::time::Duration;
-use steward_proto::{
-    self,
-    steward::{self, submit_request::CallData::AaveV2Stablecoin, SubmitRequest, SubmitResponse},
-};
-use tonic::{self, async_trait, Code, Request, Response, Status};
 
 const MESSAGE_TIMEOUT: Duration = Duration::from_secs(10);
 const CHAIN_PREFIX: &str = "somm";
-
-
-// Because of Rusts handling of enums, we have no easy way to log what cellar type and function are
-// being requested before we get to the encoding step, so we pass the whole request into this method
-// and the get_encoded_call() methods so logging can happen there.
-async fn build_cork(request: SubmitRequest) -> Result<Cork, Error> {
-    cellars::validate_cellar_id(request.cellar_id.as_str())?;
-
-    let address = request.cellar_id.clone();
-    let encoded_call = get_encoded_call(request)?;
-
-    Ok(Cork {
-        encoded_contract_call: encoded_call,
-        target_contract_address: address,
-    })
-}
-
-fn get_encoded_call(request: SubmitRequest) -> Result<Vec<u8>, Error> {
-    if request.call_data.is_none() {
-        return Err(ErrorKind::Http.context("empty contract call data").into());
-    }
-
-    match request.call_data.unwrap() {
-        AaveV2Stablecoin(call) => {
-            if call.function.is_none() {
-                return Err(ErrorKind::Http.context("empty function data").into());
-            }
-
-            aave_v2_stablecoin::get_encoded_call(call.function.unwrap(), request.cellar_id)
-        }
-    }
-}
 
 pub async fn schedule_cork(
     contract: String,
