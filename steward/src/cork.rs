2077d3bec9fc9a94ea1789ce0f00562ee3f6f588
hannydevelop <ukpaiugochiibem@gmail.com>
2022-10-13 12:33:31 +0100

    removed send_cork and cork handler

--- steward/src/cork.rs
+++ steward/src/cork.rs
@@ -22,73 +22,7 @@ use tonic::{self, async_trait, Code, Request, Response, Status};
 const MESSAGE_TIMEOUT: Duration = Duration::from_secs(10);
 const CHAIN_PREFIX: &str = "somm";
 
-pub struct CorkHandler;
 
-#[async_trait]
-impl steward::contract_call_server::ContractCall for CorkHandler {
-    async fn submit(
-        &self,
-        request: Request<SubmitRequest>,
-    ) -> Result<Response<SubmitResponse>, Status> {
-        let request = request.get_ref().to_owned();
-
-        // Check if cellar is governance approved before building cork
-        let config = APP.config();
-        let mut client = match CorkQueryClient::connect(config.cosmos.grpc.clone()).await {
-            Ok(c) => c,
-            Err(err) => {
-                error!("cork query client connection failed: {}", err);
-                return Err(Status::new(
-                    Code::Internal,
-                    format!("failed to query chain to validate cellar id: {}", err),
-                ));
-            }
-        };
-
-        debug!("checking if cellar ID is approved");
-        let ids = &client
-            .query_cellar_i_ds(QueryCellarIDsRequest {})
-            .await?
-            .get_ref()
-            .cellar_ids
-            .clone();
-        if !ids.contains(&request.cellar_id) {
-            info!(
-                "rejecting request for unapproved cellar {}",
-                request.cellar_id
-            );
-            return Err(Status::new(
-                Code::PermissionDenied,
-                format!(
-                    "cellar ID {} not approved by governance",
-                    &request.cellar_id
-                ),
-            ));
-        }
-
-        // Build and send cork
-        let cellar_id = request.cellar_id.clone();
-        let cork = match build_cork(request).await {
-            Ok(c) => c,
-            Err(err) => {
-                warn!("failed to build cork for cellar {}: {}", cellar_id, err);
-                return Err(Status::new(Code::InvalidArgument, err.to_string()));
-            }
-        };
-        debug!("cork: {:?}", cork);
-
-        if let Err(err) = send_cork(cork).await {
-            error!("failed to submit cork: {}", err);
-            return Err(Status::new(
-                Code::Internal,
-                format!("failed to send cork to sommelier: {}", err),
-            ));
-        }
-        info!("submitted cork for {}", cellar_id);
-
-        Ok(Response::new(SubmitResponse {}))
-    }
-}
 // Because of Rusts handling of enums, we have no easy way to log what cellar type and function are
 // being requested before we get to the encoding step, so we pass the whole request into this method
 // and the get_encoded_call() methods so logging can happen there.
@@ -118,28 +52,6 @@ fn get_encoded_call(request: SubmitRequest) -> Result<Vec<u8>, Error> {
             aave_v2_stablecoin::get_encoded_call(call.function.unwrap(), request.cellar_id)
         }
     }
-}
-
-async fn send_cork(cork: Cork) -> Result<TxResponse, Error> {
-    let config = APP.config();
-    debug!("establishing grpc connection");
-    let contact = Contact::new(&config.cosmos.grpc, MESSAGE_TIMEOUT, CHAIN_PREFIX)?;
-
-    debug!("getting cosmos fee");
-    let cosmos_gas_price = config.cosmos.gas_price.as_tuple();
-    let fee = Coin {
-        amount: (cosmos_gas_price.0 as u64).into(),
-        denom: cosmos_gas_price.1,
-    };
-    somm_send::send_cork(
-        &contact,
-        cork,
-        config::DELEGATE_ADDRESS.to_string(),
-        &config::DELEGATE_KEY,
-        fee,
-    )
-    .await
-    .map_err(|e| e.into())
 }
 
 pub async fn schedule_cork(
