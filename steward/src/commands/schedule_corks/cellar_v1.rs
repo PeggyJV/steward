f5c2142881d16129ac772e1afeb5d3d00429dae0
Collin Brittain <collinbrittain@gmail.com>
2022-10-19 19:36:08 -0500

    Update Cellar.sol bindings, add scheduled cork support

---
+++ steward/src/commands/schedule_corks/cellar_v1.rs
@@ -0,0 +1,27 @@
+use abscissa_core::{clap::Parser, Command, Runnable};
+
+mod initiate_shutdown;
+mod lift_shutdown;
+mod reset_high_watermark;
+mod set_fees_distributor;
+mod set_owner;
+mod set_performance_fee;
+mod set_platform_fee;
+mod set_strategist_performance_cut;
+mod set_strategist_platform_cut;
+mod trust_position;
+
+/// Schedule shutdown commands
+#[derive(Command, Debug, Parser, Runnable)]
+pub enum CellarV1Cmd {
+    InitiateShutdown(initiate_shutdown::InitiateShutdownCmd),
+    LiftShutdown(lift_shutdown::LiftShutdownCmd),
+    ResetHighWatermark(reset_high_watermark::ResetHighWatermarkCmd),
+    SetFeesDistributor(set_fees_distributor::SetFeesDistributorCmd),
+    SetOwner(set_owner::SetOwnerCmd),
+    SetPerformanceFee(set_performance_fee::SetPerformanceFeeCmd),
+    SetPlatformFee(set_platform_fee::SetPlatformFeeCmd),
+    SetStrategistPerformanceCut(set_strategist_performance_cut::SetStrategistPerformanceCutCmd),
+    SetStrategistPlatformCut(set_strategist_platform_cut::SetStrategistPlatformCutCmd),
+    TrustPosition(trust_position::TrustPositionCmd),
+}
---
+++ steward/src/commands/schedule_corks/cellar_v1/set_fees_distributor.rs
@@ -0,0 +1,58 @@
+use crate::{application::APP, cellars, cork, prelude::*};
+use abscissa_core::{clap::Parser, Command, Runnable};
+use ethers::abi::{AbiEncode, Address};
+use steward_abi::cellar::*;
+
+/// Fees Distributor subcommand
+#[derive(Command, Debug, Parser)]
+#[clap(
+    long_about = "DESCRIPTION\n\nCalls setFeesDistributor() on the target cellar contract at the specified block height.\nFor more information see https://github.com/PeggyJV/cellar-contracts/blob/main/src/base/Cellar.sol"
+)]
+pub struct SetFeesDistributorCmd {
+    #[clap(short = 'n', long)]
+    /// Fee distributor's address
+    new_fees_distributor: Address,
+
+    /// Target cellar for scheduled cork.
+    #[clap(short, long)]
+    cellar_id: String,
+
+    /// Block height to schedule cork.
+    #[clap(short = 'b', long)]
+    height: u64,
+}
+
+impl Runnable for SetFeesDistributorCmd {
+    fn run(&self) {
+        abscissa_tokio::run_with_actix(&APP, async {
+            let mut address = self.new_fees_distributor.as_bytes().to_vec();
+
+            while address.len() < 32 {
+                address.insert(0, 0u8);
+            }
+
+            let mut address_slice: [u8; 32] = Default::default();
+            address_slice.copy_from_slice(&address[..]);
+            let call = SetFeesDistributorCall {
+                new_fees_distributor: address_slice,
+            };
+            let encoded_call = CellarCalls::SetFeesDistributor(call).encode();
+
+            cellars::validate_cellar_id(&self.cellar_id).unwrap_or_else(|err| {
+                status_err!("invalid cellar ID: {}", err);
+                std::process::exit(1);
+            });
+
+            cork::schedule_cork(self.cellar_id.clone(), encoded_call, self.height)
+                .await
+                .unwrap_or_else(|err| {
+                    status_err!("error while submitting scheduled cork. please verify that the scheduling failed before attempting again: {}", err);
+                    std::process::exit(1);
+                })
+        })
+        .unwrap_or_else(|e| {
+            status_err!("executor exited with error: {}", e);
+            std::process::exit(1);
+        });
+    }
+}
