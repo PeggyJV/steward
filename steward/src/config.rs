5eab8e10cc5b485bdbb145902d2623ef4ed59ce3
Collin Brittain <collinbrittain@gmail.com>
2023-05-09 13:40:47 -0500

    Fix CosmosSection config order to make config test happy

--- steward/src/config.rs
+++ steward/src/config.rs
@@ -171,8 +171,8 @@ pub struct CosmosSection {
     pub key_derivation_path: String,
     pub prefix: String,
     pub msg_batch_size: u32,
-    pub gas_price: GasPrice,
     pub gas_limit_per_msg: u64,
+    pub gas_price: GasPrice,
 }
 
 impl Default for CosmosSection {
@@ -183,8 +183,8 @@ impl Default for CosmosSection {
             key_derivation_path: "m/44'/118'/0'/0/0".to_owned(),
             prefix: "somm".to_owned(),
             msg_batch_size: 5,
-            gas_price: GasPrice::default(),
             gas_limit_per_msg: 2_000_000u64,
+            gas_price: GasPrice::default(),
         }
     }
 }
