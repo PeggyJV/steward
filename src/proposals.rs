d4e76648523afc11300f3bcd67761fbfb49afd7c
Zaki Manian <zaki@iqlusion.io>
2024-08-09 09:58:54 -0400

    Update last finalized proposal id

--- src/proposals.rs
+++ src/proposals.rs
@@ -194,6 +194,8 @@ async fn poll_approved_proposals(
                 } else if err.code() == Code::Unknown {
                     error!("error querying proposal {}: {}", proposal_id, err);
 
+                    state.last_finalized_proposal_id = proposal_id;
+
                     continue;
                 } else {
                     return Err(proposal_processing_error(format!(
