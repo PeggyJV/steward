309d3494db348ba7f79591b5d02e94a67a987a8f
Collin Brittain <collinbrittain@gmail.com>
2024-11-15 09:52:12 -0600

    Generate go protos

--- scripts/build_go_protos.sh
+++ scripts/build_go_protos.sh
@@ -10,8 +10,10 @@ echo
 echo Building Go bindings. Output will be in ./steward_proto_go/steward_proto
 mkdir -p $GO_OUT
 protoc --proto_path=$PROTO_PATH \
+	--proto_path=$PROTO_PATH/boring_vault/v1 \
 	--go_out=$GO_OUT/ \
 	--go-grpc_out=$GO_OUT/ \
     $PROTO_PATH/*.proto \
+	$PROTO_PATH/boring_vault/v1/*.proto
 
 echo Done!
